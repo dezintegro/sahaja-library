3be02c889940501e4fea5f6c4b3c0772e200b04f
dezintegro <dezintegro@gmail.com>
2024-02-10 21:42:52 +0700

    refactor(back): remove duplicated stuff from date_parser.py

--- api-backend/core/date_parser.py
+++ api-backend/core/date_parser.py
@@ -2,48 +2,16 @@ from typing import Optional
 
 from natasha import MorphVocab
 from natasha.extractors import Extractor
-from natasha.grammars.date import Date
-from yargy import rule, and_, or_
-from yargy.predicates import (
-    eq,
-    gte,
-    lte,
-    length_eq,
-    dictionary,
-    normalized,
+from natasha.grammars.date import (
+    Date,
+    DAY,
+    MONTH,
+    YEAR,
+    YEAR_SHORT,
+    YEAR_WORD,
+    MONTH_NAME,
 )
-
-MONTHS = {
-    "январь": 1,
-    "февраль": 2,
-    "март": 3,
-    "апрель": 4,
-    "май": 5,
-    "июнь": 6,
-    "июль": 7,
-    "август": 8,
-    "сентябрь": 9,
-    "октябрь": 10,
-    "ноябрь": 11,
-    "декабрь": 12,
-}
-
-
-MONTH_NAME = dictionary(MONTHS).interpretation(
-    Date.month.normalized().custom(MONTHS.__getitem__)
-)
-
-MONTH = and_(gte(1), lte(12)).interpretation(Date.month.custom(int))
-
-DAY = and_(gte(1), lte(31)).interpretation(Date.day.custom(int))
-
-YEAR_WORD = or_(rule("г", eq(".").optional()), rule(normalized("год")))
-
-YEAR = and_(gte(1000), lte(2100)).interpretation(Date.year.custom(int))
-
-YEAR_SHORT = and_(length_eq(2), gte(0), lte(99)).interpretation(
-    Date.year.custom(lambda _: 1900 + int(_))
-)
+from yargy import rule, or_
 
 DATA_DELIMITER = or_(
     rule("."),
