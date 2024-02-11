2885ab8904a0908b65cf5c6652cfa5f009d5a36f
dezintegro <dezintegro@gmail.com>
2024-02-12 01:11:56 +0700

    feat(front): add yandex metrika

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Suspense } from 'react'
+import { Metrika } from '../src/components/Metrika'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Suspense>
+        <Metrika />
+      </Suspense>
+      <Component {...pageProps} />
+    </>
+  )
 }
