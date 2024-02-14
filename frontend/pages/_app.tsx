3e2b1b3b15e9ca581084fffbc22380be127bea75
dezintegro <dezintegro@gmail.com>
2024-02-14 13:23:06 +0700

    refactor(front): move head from main  page to app.tsx, add og meta

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Suspense } from 'react'
 import { Metrika } from '../src/components/Metrika'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -9,6 +10,16 @@ export default function App({ Component, pageProps }: AppProps) {
       <Suspense>
         <Metrika />
       </Suspense>
+      <Head>
+        <title>Сахаджа Йога - Библиотека лекций Шри Матаджи Нирмала Деви</title>
+        <meta
+          name="description"
+          content="Сахаджа Библиотека - поиск по лекциям Шри Матаджи Нирмала Деви. Введите запрос и найдите лекции по интересующей теме."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:image" content="/og-image.jpg" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </>
   )
