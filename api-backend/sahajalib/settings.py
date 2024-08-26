051deb3b6cb73fc9476b5c463cc4d2c2589addaa
dezintegro <dezintegro@gmail.com>
2024-08-26 22:16:12 +0700

    feat(api): add page size pagination support

--- api-backend/sahajalib/settings.py
+++ api-backend/sahajalib/settings.py
@@ -145,7 +145,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "DEFAULT_PAGINATION_CLASS": "core.pagination.PageNumberWithSizePagination",
     "PAGE_SIZE": 10,
     # "PAGE_SIZE": None,
 }
