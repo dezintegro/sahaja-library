b866e034c6ebfe26bd7866ee1b57b4111573280c
dezintegro <dezintegro@gmail.com>
2024-10-14 13:26:47 +0700

    feat(ops): add handle django admin static

--- api-backend/sahajalib/urls.py
+++ api-backend/sahajalib/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
@@ -26,3 +28,5 @@ urlpatterns = [
     path("api/", include("modules.catalog.urls")),
     path('sitemap.xml', cache_page(86400)(sitemap), {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
+
+urlpatterns += static(settings.STATIC_URL)
